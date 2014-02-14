44956ccd6af957c078f0c26a904a411fe19f61c2
lorinbeer <lorin@adobe.com>
2014-02-13 17:47:12 -0800

    cleaner use of cordova cli through child shell
    creates project and adds platforms

--- src/util.js
+++ src/util.js
@@ -3,29 +3,46 @@ var exec = require('child_process').exec,
     cordova = require('cordova');
 
 module.exports = {
+    /**
+     *
+     */
     createTestBench : function(sha) {
+        var child,
+            projName = 'TestBench-' + sha,
+            projId = 'org.testbench.cordova';
+
         console.log("utils createTestBench for commit ", sha);
-        var child = exec('cordova create TestBench-'+sha+' com.cordova.testbench '+'TEST-BENCH-'+sha, function(err,stdout,stderr) {
-            console.log(err,stdout,stderr);
 
+        child = exec('cordova create ' + projName  + ' ' + projId + ' ' + projName, function(err,stdout,stderr) {
+            console.log("cordova project created successfully", err);
+        
+           process.chdir(projName);              
+           exec('pwd', function(e,out,err) {console.log(out);});
+    
+           exec('cordova platform add android', function(e,out,err){
+                console.log(e,out,err);
+            
+//                exec('cordova platform add android', function(err,stdout,stderr) {
+ //                 console.log("WHAT THE FUCK, CALL ME called promise");
+//                });
+                console.log("exec has been called");
+            });     
 
+       });
 
-
-
-
-
-
-cordova.platform('add', 'android', function(e) {
-        if (e) {
-            self.phonegap.emit('error', e);
-            callback(e);
-            return;
-        }
-});
-
+/*function(e) {
+                console.log('test adding, did it work?', e);
+                if (e) {
+                    self.phonegap.emit('error', e);
+                    callback(e);
+                    return;
+                }
+//            }*///.then(function (a,b,c) {console.log("add called promise",a,b,c);});
    //            var child1 = exec('cordova plugin add ' + path-to-allplugins);
-        });
     },
+    /**
+     * 
+     */
     checkoutCommit : function(sha,cb) {
         child = exec('git checkout ' + sha, function(err,stdout,stderr) {
             console.log(err,stdout,stderr);
