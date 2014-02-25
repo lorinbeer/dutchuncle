8993f6fdcb3d23d537c262d6aedde7918f7e78f9
lorinbeer <lorin@adobe.com>
2014-02-25 10:43:09 -0800

    updated tests with properly mocked child_process.exec object
    added tests for gitCheckout util method
    comparison failure with one test

--- spec/utilSpec.js
+++ spec/utilSpec.js
@@ -1,18 +1,17 @@
 
 var Q = require('Q'),
-    path = require('path');
-
-
+    path = require('path')
+    child = require('child_process');
+    
 describe("DU Util Test Suite", function () {
     var util = require('../src/util.js'),
         aBigFatLie = {},
-        processMock = {},
-        exec = function(cmd,cb) {
-        }; 
-            
-
+        config = { 'temproot' : "~/.cordovatestsuite" },
+        mockExecPass,
+        mockExecFail; 
 
     beforeEach(function () {
+        
         aBigFatLie = {
         'promise' : "I PROMISE",
         'resolve' : function() {},
@@ -24,30 +23,75 @@ describe("DU Util Test Suite", function () {
 
         spyOn(process,'chdir');
         spyOn(Q, 'defer');
+/*
+        mockExecPass = jasmine.createSpy('exec').andCallFake(function(cmd,cb) {
+            cb(null,"",null);
+        });
+*/
     });
 
 
-    it("should have an 'addCordovaPlatform' function which returns a promise", function () {
-        var promise;
-        expect(util.addCordovaPlatform).toBeDefined();
-        promise = util.addCordovaPlatform("","");
-        expect(promise).toBeDefined();
+    describe("checkoutGitCommit unit tests", function() {
+        var sha,
+            cb,
+            mockExecPass; 
+
+        beforeEach(function () {
+            sha = "whatarethechances"
+            cb = function (err,stdout,stderr) {};
+ 
+         // mock config object
+            config = { 'temproot' : '/Users/defaultuser/.cordovatestsuite' },
+            spyOn(config,'temproot'); 
+         
+            spyOn(util,'checkoutGitCommit').andCallThrough();
+
+            mockExecPass = spyOn(child, 'exec');
+
+            util.checkoutGitCommit(sha,cb);
+        });
+    
+        it("should have an 'checkoutGitCommit'", function () {
+            expect(util.checkoutGitCommit).toBeDefined();
+        });
+
+        it("should attempt to checkout a git commit through executing a shell command", function () {
+            expect(util.checkoutGitCommit).toHaveBeenCalledWith(sha,cb);
+            expect(mockExecPass).toHaveBeenCalledWith("git checkout " + sha); 
+        });
+
     });
 
-    it("should attempt to change the working directory when 'addCordovaPlatform' is called", function () {
-        var testPath = path.join(process.cwd(),"path/to/cordova/project");
-        
-        promise = util.addCordovaPlatform(testPath, "android");
-        expect(process.chdir).toHaveBeenCalled();
+   describe("addCordovaPlatform unit tests", function() {
+        var testPath = "path/to/cordova/project",
+            testPlatform = "nintendo-play-360-box", 
+            config,
+            promise;
+ 
+
+        beforeEach(function () {
+         // mock config object
+            config = { 'temproot' : '/Users/defaultuser/.cordovatestsuite' },
+            spyOn(config,'temproot'); 
+         // mock the temp dir generation function in utils  
+            spyOn(util,'getTempDirPath').andReturn("/Users/guy/.cordovatestsuite");
+            spyOn(util,'addCordovaPlatform').andCallThrough();
+ 
+            promise = util.addCordovaPlatform(testPath, testPlatform);
+
+        });
+    
+        it("should have an 'addCordovaPlatform' function which returns a promise", function () {
+            expect(util.addCordovaPlatform).toHaveBeenCalledWith(testPath,testPlatform);
+            expect(promise).toBeDefined();
+        });
+
+        it("should change working directories to the test suite temp directory offset by a valid cordova project path", function () {
+            expect(process.chdir).toHaveBeenCalledWith(path.join(util.getTempDirPath(),testPath));
+        });
+
     });
 
-    it("should reject the promise when exec fails", function () {
-        
-    });
-
-    it("should resolve the promise when exec succeeds", function () {
-
-    });
 
 
 });
