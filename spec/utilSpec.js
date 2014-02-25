22a904f50e1d7e487914e8cc80f374f6a64441e6
lorinbeer <lorin@adobe.com>
2014-02-25 11:50:37 -0800

    added test suite for buildCordovaProject command

--- spec/utilSpec.js
+++ spec/utilSpec.js
@@ -57,12 +57,82 @@ describe("DU Util Test Suite", function () {
 
         it("should attempt to checkout a git commit through executing a shell command", function () {
             expect(util.checkoutGitCommit).toHaveBeenCalledWith(sha,cb);
-            expect(mockExecPass).toHaveBeenCalledWith("git checkout " + sha); 
+            expect(mockExecPass).toHaveBeenCalled();
+            expect(mockExecPass.mostRecentCall.args[0]).toEqual('git checkout ' + sha);
         });
 
     });
 
-   describe("addCordovaPlatform unit tests", function() {
+    //
+    describe("Cordova Utility Function Unit Tests", function() {
+        var testPath = "path/to/cordova/project",
+            testPlatform = "nintendo-play-360-box", 
+            config,
+            promise;
+ 
+
+        beforeEach(function () {
+         // mock config object
+            config = { 'temproot' : '/Users/defaultuser/.cordovatestsuite' },
+            spyOn(config,'temproot'); 
+         // mock the temp dir generation function in utils  
+            spyOn(util,'getTempDirPath').andReturn("/Users/guy/.cordovatestsuite");
+
+        });
+   
+
+        describe("'addCordovaPlatform Unit Tests", function () {
+            var promise;
+            
+            beforeEach(function () {
+            // create spy and run function
+               spyOn(util,'addCordovaPlatform').andCallThrough();
+               promise = util.addCordovaPlatform(testPath, testPlatform);
+
+            });
+     
+            it("should have an 'addCordovaPlatform' function which returns a promise", function () {
+                expect(util.addCordovaPlatform).toHaveBeenCalledWith(testPath,testPlatform);
+                expect(promise).toBeDefined();
+            });
+
+            it("should change working directories to the test suite temp directory offset by a valid cordova project path", function () {
+                expect(process.chdir).toHaveBeenCalledWith(path.join(util.getTempDirPath(),testPath));
+            });
+
+        });
+
+        describe("'buldCordovaProject Unit Tests", function () {
+            var promise;
+            
+            beforeEach(function () {
+            // create spy and run function
+               spyOn(util,'buildCordovaProject').andCallThrough();
+               promise = util.buildCordovaProject(testPath, testPlatform);
+
+            });
+     
+            it("should have an 'addCordovaPlatform' function which returns a promise", function () {
+                expect(util.buildCordovaProject).toHaveBeenCalledWith(testPath,testPlatform);
+                expect(promise).toBeDefined();
+            });
+
+            it("should change working directories to the test suite temp directory offset by a valid cordova project path", function () {
+                expect(process.chdir).toHaveBeenCalledWith(path.join(util.getTempDirPath(),testPath));
+            });
+
+        });
+
+
+
+
+
+
+
+
+    });
+
+   describe("buildCordovaProject unit tests", function() {
         var testPath = "path/to/cordova/project",
             testPlatform = "nintendo-play-360-box", 
             config,
